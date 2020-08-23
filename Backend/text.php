a59989876ae3cc19b2e2eed68c616a532b227e31
Joel-Gomez-12 <68835851+Joel-Gomez-12@users.noreply.github.com>
2020-08-23 00:34:00 -0500

    Delete text.php

--- Backend/text.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "Sha1 ".sha1("lucresia")."<br>";
-    echo "Sha1 ".sha1("molineros")."<br>";
-    echo "Sha1 ".sha1("insecto")."<br>";
-
-    echo "Unique ".sha1(uniqid(rand(), true));
-?>
